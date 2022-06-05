aff96928e48d10488de78108e91a253ee57e71b3
TheLogicMaster <justinmarentette11@gmail.com>
2022-06-05 19:45:59 -0400

    Joystick support

--- core/src/com/ychstudio/systems/PlayerSystem.java
+++ core/src/com/ychstudio/systems/PlayerSystem.java
@@ -6,6 +6,7 @@ import com.artemis.Entity;
 import com.artemis.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
@@ -68,7 +69,7 @@ public class PlayerSystem extends IteratingSystem {
             }
 
             // player movement controls
-            if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.UP) || Controllers.getCurrent().getAxis(1) > 0.1f) {
                 if (player.invincible || !hitBombVertical(body, fromV.set(body.getPosition()), toV.set(body.getPosition().x, body.getPosition().y + 0.5f))) {
                     if (Math.abs(linearVelocity.y) < maxSpeed) {
                         body.applyLinearImpulse(new Vector2(0, player.acceleration * body.getMass()), body.getWorldCenter(), true);
@@ -78,7 +79,7 @@ public class PlayerSystem extends IteratingSystem {
                 player.state = Player.State.WALKING_UP;
             }
 
-            if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.DOWN)|| Controllers.getCurrent().getAxis(1) < -0.1f) {
                 if (player.invincible || !hitBombVertical(body, fromV.set(body.getPosition()), toV.set(body.getPosition().x, body.getPosition().y - 0.5f))) {
                     if (Math.abs(linearVelocity.y) < maxSpeed) {
                         body.applyLinearImpulse(new Vector2(0, -player.acceleration * body.getMass()), body.getWorldCenter(), true);
@@ -88,7 +89,7 @@ public class PlayerSystem extends IteratingSystem {
                 player.state = Player.State.WALKING_DOWN;
             }
 
-            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)|| Controllers.getCurrent().getAxis(0) < -0.1f) {
                 if (player.invincible || !hitBombHorizontal(body, fromV.set(body.getPosition()), toV.set(body.getPosition().x - 0.5f, body.getPosition().y))) {
                     if (Math.abs(linearVelocity.x) < maxSpeed) {
                         body.applyLinearImpulse(new Vector2(-player.acceleration * body.getMass(), 0), body.getWorldCenter(), true);
@@ -98,7 +99,7 @@ public class PlayerSystem extends IteratingSystem {
                 player.state = Player.State.WALKING_LEFT;
             }
 
-            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)|| Controllers.getCurrent().getAxis(0) > 0.1f) {
                 if (player.invincible || !hitBombHorizontal(body, fromV.set(body.getPosition()), toV.set(body.getPosition().x + 0.5f, body.getPosition().y))) {
                     if (Math.abs(linearVelocity.x) < maxSpeed) {
                         body.applyLinearImpulse(new Vector2(player.acceleration * body.getMass(), 0), body.getWorldCenter(), true);
